03686d7db180c98b6b0ceeae5d28741a50432c78
amaan <amaanraza900@gmail.com>
2022-11-22 19:28:51 +0530

    pyhon4

---
+++ file4.py
@@ -0,0 +1,76 @@
+# a-z A-Z 0-9 _ 
+
+name = "New Delhi"
+Name = "Mumbai"
+#print(Name)
+"""
+x = 10
+y = 10
+z = 10
+print(x)
+print(y)
+print(z)
+"""
+"""
+x = y = z = 10
+
+print(x)
+print(y)
+print(z)
+"""
+#x , y, z = 10, 20, 30
+#print(x)
+#print(y)
+#print(z)
+
+#name , qty , price =  "Banana", 20, 100.50
+
+age = 20
+print(type(age))
+
+name = "Waris"
+print(type(name))
+
+price = 10.40
+print(type(price))
+
+x = [10, 40, 50, 50, 50]
+#print(type(x))
+#casting
+#Float to Int
+x =  20.50
+#print( int(x) )
+y = int(x)
+print(y)
+print(type(y))
+#Int to Float
+a = 100
+print(float(a))
+b = float(a)
+print(b)
+print(type(b))
+
+#Int to str
+a = 100
+print(str(a))
+b = str(a)
+
+print(b)
+print(type(b))
+
+name = "100"
+print(float(name))
+
+x = 10
+print("===================")
+print(x)
+del x
+print(x)
+y = 20.0
+z = x + y
+print(z)
+str1 = " New Delhi "
+str2 = " India "
+s = str1 + str2
+#s = str1 + str(x)
+print(s)
